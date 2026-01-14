f6b9143ab932757356a756615ab507f67fabbe1b
natcore-tech <asesoriaelectr@gmail.com>
2026-01-14 10:16:33 -0500

    fix main

--- src/lab/LabRbForm.tsx
+++ src/lab/LabRbForm.tsx
@@ -1,4 +1,4 @@
-    import React, { useState } from "react";
+import React, { useState } from "react";
 import { Container } from "react-bootstrap";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
